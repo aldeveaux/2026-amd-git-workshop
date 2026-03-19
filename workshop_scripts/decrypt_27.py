5708dc9c59b2b28acded2719cfdd8b48da846514
Brannen <wbrannen@lp5dphslwgs01p.eads.ncads.net>
2026-03-19 11:51:56 -0400

    decrypt key 27

---
+++ workshop_scripts/decrypt_27.py
@@ -0,0 +1,31 @@
+import os
+
+my_key=27
+
+# Simple function to decrypt a string by shifting the ascii code by some value (key)
+def decrypt(encrypted_message:str) -> str:
+    message = ""
+    for i in range(len(encrypted_message)):
+        char = encrypted_message[i]
+        decrypted_char = chr((ord(char) - my_key) % 126)
+        message += decrypted_char
+    return message
+
+
+def get_relative_path(my_path:str):
+    current_dir = os.path.dirname(__file__)
+    return os.path.join(current_dir, my_path, f"{my_key}.txt")
+
+
+def main():
+    in_path = get_relative_path("../encrypted_data")
+    out_path = get_relative_path("../workshop_data")
+
+    encrypted_file = open(in_path, "r")
+
+    with open(out_path, "w") as decrypted_file:
+        decrypted_file.write(decrypt(encrypted_file.read()))
+
+
+if __name__ == "__main__":
+    main()
